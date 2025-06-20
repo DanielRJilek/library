10a58d340ed4e2afc3d6bf7e4f1a347897b0e9d2
Daniel <jilek.d@northeastern.edu>
2025-06-20 19:37:39 -0400

    Basic js functions written for testing.

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read, id) {
+function Book(title, author, pages, id, progress) {
     this.title = title;
     this.author = author;
     this.pages = pages; 
-    this.read = read;
+    this.read = true;
     this.id = id; 
+    this.progress = progress;
 }
 
-function addBooktoLibrary(title, author, pages, read, id) {
-    book = Book(title, author, pages, read, id);
+function addBooktoLibrary(title, author, pages, id, progress) {
+    const book = new Book(title, author, pages, id, progress);
     myLibrary.push(book);
 }
 
 function displayBooks(library) {
-    
+    const content = document.querySelector('#content')
+    for (let book in library) {
+        const div = document.createElement("div");
+        div.classList.add("book");
+        div.textContent = book.title;
+
+
+        const readButton = document.createElement("button");
+        readButton.classList.add("readButton");
+        readButton.textContent = "Mark Read";
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("removeButton");
+        removeButton.textContent = "Remove";
+        div.appendChild(readButton);
+        div.appendChild(removeButton);
+
+
+        content.appendChild(div);
+    }
 }
+
+addBooktoLibrary("The Three Musketeers", "Alexandre Dumas", 100, crypto.randomUUID(), false);
+displayBooks(myLibrary);
